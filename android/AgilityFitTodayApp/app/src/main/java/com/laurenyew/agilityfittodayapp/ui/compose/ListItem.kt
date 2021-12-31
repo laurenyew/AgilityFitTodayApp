9f4605a40aebfc2edba2cba0adb6638c16ee21b2
Lauren Yew <laurenyew@gmail.com>
2021-12-30 20:09:15 -0600

    Adding initial home page

---
+++ android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/compose/ListItem.kt
@@ -0,0 +1,47 @@
+package com.laurenyew.agilityfittodayapp.ui.compose
+
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.runtime.State
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun ListItem(
+    title: String,
+    description: String,
+    isFavorite: State<Boolean>? = null,
+    onItemFavorited: ((Boolean) -> Unit)? = null,
+    modifier: Modifier = Modifier,
+) {
+    Row(modifier = modifier) {
+        val imageModifier = Modifier
+            .size(72.dp)
+            .padding(2.dp)
+            .fillMaxSize()
+
+        Column(
+            modifier = Modifier
+                .weight(1f)
+                .padding(2.dp)
+                .align(Alignment.CenterVertically)
+        ) {
+            Text(title, style = MaterialTheme.typography.body1)
+            Spacer(modifier = Modifier.height(3.dp))
+            Text(description)
+        }
+
+        if (isFavorite != null) {
+            Spacer(modifier = Modifier.width(5.dp))
+            FavoriteButton(
+                isFavorite = isFavorite.value,
+                onItemFavorited = onItemFavorited,
+                modifier = imageModifier
+            )
+            Spacer(modifier = Modifier.width(8.dp))
+        }
+    }
+}
---
+++ android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/home/HomeScreen.kt
@@ -0,0 +1,82 @@
+package com.laurenyew.agilityfittodayapp.ui.home
+
+import android.widget.Toast
+import androidx.compose.foundation.layout.*
+import androidx.compose.material.Button
+import androidx.compose.material.MaterialTheme
+import androidx.compose.material.Text
+import androidx.compose.runtime.Composable
+import androidx.compose.ui.Alignment
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.platform.LocalContext
+import androidx.compose.ui.text.style.TextAlign
+import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+
+@Composable
+fun HomeScreen() {
+    val context = LocalContext.current
+
+    Column(
+        Modifier.fillMaxSize(),
+        horizontalAlignment = Alignment.CenterHorizontally
+    ) {
+        Text(
+            text = "Welcome to the Agility Fit Today App!",
+            textAlign = TextAlign.Center,
+            style = MaterialTheme.typography.subtitle1,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Text(
+            text = "This sample app features fitness workout creations that demo Jetpack libraries, Kotlin Coroutines & Flows, and other Android tech / architecture.",
+            style = MaterialTheme.typography.subtitle1,
+            textAlign = TextAlign.Center,
+            modifier = Modifier
+                .padding(8.dp)
+        )
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            Toast.makeText(context, "Start a Workout", Toast.LENGTH_LONG).show()
+        }) {
+            Text(
+                text = "Start a Workout",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.subtitle1,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            Toast.makeText(context, "Create a Workout", Toast.LENGTH_LONG).show()
+        }) {
+            Text(
+                text = "Create a Workout",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.subtitle1,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+        Spacer(modifier = Modifier.height(16.dp))
+        Button(onClick = {
+            Toast.makeText(context, "Old Workouts", Toast.LENGTH_LONG).show()
+        }) {
+            Text(
+                text = "Old Workouts",
+                textAlign = TextAlign.Center,
+                style = MaterialTheme.typography.subtitle1,
+                modifier = Modifier
+                    .padding(8.dp)
+            )
+        }
+    }
+
+}
+
+@Preview
+@Composable
+fun HomeScreenPreview() {
+    HomeScreen()
+}
