0ecc7dabeee0ad999bb87f6c63794e7d494d9d5c
Lauren Yew <laurenyew@gmail.com>
2022-12-27 20:53:29 -0600

    IOS: Fixing unit tests

--- ios/AgilityFitTodayApp/AgilityFitTodayApp/Workout/Select/SelectWorkoutViewModel.swift
+++ ios/AgilityFitTodayApp/AgilityFitTodayApp/Workout/Select/SelectWorkoutViewModel.swift
@@ -9,14 +9,19 @@ import Foundation
 import SwiftUI
 
 class SelectWorkoutViewModel: ObservableObject {
-    private let workoutRepo = AppEnvironment.current.workoutRepository
     
     @Published
     private(set) var workoutSet: WorkoutSet = []
     @Published
     private(set) var workoutTypes: [WorkoutType] = []
     
-    init() {
+    // TODO: This doesn't seem right. How should we properly pass around the workout repository?
+    convenience init() {
+        let workoutRepo = AppEnvironment.current.workoutRepository
+        self.init(workoutRepo: workoutRepo)
+    }
+    
+    init(workoutRepo: WorkoutRepository) {
         workoutSet = workoutRepo.workoutSetSortedByType
         workoutTypes = workoutRepo.workoutTypes
     }
