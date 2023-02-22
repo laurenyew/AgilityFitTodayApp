3c9971032d5defcba51a64d6fe7efec1929b0026
Lauren Yew <laurenyew@gmail.com>
2023-02-21 18:59:54 -0600

    TECH-6: Cleaning up icons

--- android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/devsettings/DevSettingsLandingScreen.kt
+++ android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/devsettings/DevSettingsLandingScreen.kt
@@ -7,10 +7,14 @@ import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.height
 import androidx.compose.foundation.layout.padding
 import androidx.compose.material.Button
+import androidx.compose.material.Icon
+import androidx.compose.material.IconButton
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
 import androidx.compose.material.TopAppBar
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.ArrowBack
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.getValue
 import androidx.compose.runtime.mutableStateOf
@@ -38,6 +42,10 @@ fun DevSettingsNavHost(
         topBar = {
             TopAppBar(title = {
                 Text(text = title)
+            }, navigationIcon = {
+                IconButton(onClick = { navController.popBackStack() }) {
+                    Icon(imageVector = Icons.Filled.ArrowBack, contentDescription = "Back")
+                }
             })
 
         }
--- android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/startWorkout/execute/ExecuteWorkoutScreen.kt
+++ android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/startWorkout/execute/ExecuteWorkoutScreen.kt
@@ -1,7 +1,5 @@
 package com.laurenyew.agilityfittodayapp.ui.startWorkout.execute
 
-import androidx.annotation.DrawableRes
-import androidx.compose.foundation.Image
 import androidx.compose.foundation.layout.Arrangement
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
@@ -13,21 +11,24 @@ import androidx.compose.foundation.layout.size
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.FabPosition
 import androidx.compose.material.FloatingActionButton
+import androidx.compose.material.Icon
 import androidx.compose.material.MaterialTheme
 import androidx.compose.material.Scaffold
 import androidx.compose.material.Text
+import androidx.compose.material.icons.Icons
+import androidx.compose.material.icons.filled.Flag
+import androidx.compose.material.icons.filled.Pause
+import androidx.compose.material.icons.filled.PlayArrow
 import androidx.compose.runtime.Composable
 import androidx.compose.runtime.collectAsState
 import androidx.compose.runtime.getValue
 import androidx.compose.ui.Alignment
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.layout.ContentScale
-import androidx.compose.ui.res.painterResource
+import androidx.compose.ui.graphics.vector.ImageVector
 import androidx.compose.ui.tooling.preview.Preview
 import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import androidx.hilt.navigation.compose.hiltViewModel
-import com.laurenyew.agilityfittodayapp.R
 import com.laurenyew.agilityfittodayapp.ui.startWorkout.StartWorkoutFlowViewModel
 
 @Composable
@@ -74,22 +75,34 @@ fun ExecuteWorkoutControls(
     ) {
         when (workoutState) {
             WorkoutExecutionState.NOT_STARTED ->
-                ExecuteWorkoutFAB(fabTitle = "Start", fabIcon = R.drawable.ic_play) {
+                ExecuteWorkoutFAB(
+                    fabTitle = "Start",
+                    imageVector = Icons.Filled.PlayArrow
+                ) {
                     updateWorkoutState(WorkoutExecutionState.IN_PROGRESS)
                 }
 
             WorkoutExecutionState.IN_PROGRESS ->
-                ExecuteWorkoutFAB(fabTitle = "Pause", fabIcon = R.drawable.ic_pause) {
+                ExecuteWorkoutFAB(
+                    fabTitle = "Pause",
+                    imageVector = Icons.Filled.Pause
+                ) {
                     updateWorkoutState(WorkoutExecutionState.STOPPED)
                 }
 
             WorkoutExecutionState.STOPPED -> {
-                ExecuteWorkoutFAB(fabTitle = "Resume", fabIcon = R.drawable.ic_play) {
+                ExecuteWorkoutFAB(
+                    fabTitle = "Resume",
+                    imageVector = Icons.Filled.PlayArrow
+                ) {
                     updateWorkoutState(WorkoutExecutionState.IN_PROGRESS)
                 }
 
                 Spacer(Modifier.width(30.dp))
-                ExecuteWorkoutFAB(fabTitle = "Finish", fabIcon = R.drawable.ic_flag) {
+                ExecuteWorkoutFAB(
+                    fabTitle = "Finish",
+                    Icons.Filled.Flag
+                ) {
                     updateWorkoutState(WorkoutExecutionState.CANCELLED)
                 }
             }
@@ -104,7 +117,7 @@ fun ExecuteWorkoutControls(
 @Composable
 fun ExecuteWorkoutFAB(
     fabTitle: String,
-    @DrawableRes fabIcon: Int,
+    imageVector: ImageVector,
     onFABClicked: () -> Unit
 ) {
     FloatingActionButton(
@@ -115,12 +128,11 @@ fun ExecuteWorkoutFAB(
                 .size(36.dp)
                 .fillMaxSize()
 
-            Image(
-                painter = painterResource(id = fabIcon),
-                contentDescription = null,
-                contentScale = ContentScale.FillWidth,
-                alignment = Alignment.Center,
-                modifier = imageModifier
+            Icon(
+                imageVector = imageVector,
+                contentDescription = fabTitle,
+                modifier = imageModifier,
+                tint = MaterialTheme.colors.onPrimary
             )
 
             Text(
