a8d8525897b42887d9fee2c77c7a18c42fc5ce5f
Lauren Yew <laurenyew@gmail.com>
2023-04-08 15:55:05 -0500

    TECH-6: Add basic navigation button UI

--- android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/devsettings/animation/NavAnimationScreen.kt
+++ android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/devsettings/animation/NavAnimationScreen.kt
@@ -1,19 +1,69 @@
 package com.laurenyew.agilityfittodayapp.ui.devsettings.animation
 
+import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.Row
+import androidx.compose.foundation.layout.Spacer
+import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.padding
+import androidx.compose.foundation.layout.width
+import androidx.compose.material.Button
+import androidx.compose.material.ButtonDefaults
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.ui.Modifier
+import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.tooling.preview.Preview
+import androidx.compose.ui.unit.dp
+import timber.log.Timber
 
 /**
  * Animation screens showing button / card animation into a new screen
  */
 @Composable
 fun NavAnimationScreen() {
-    Text("Navigation")
+    Column {
+        RowOfBoxes(numBoxes = 1)
+        RowOfBoxes(numBoxes = 3, boxColor = Color.Green)
+        RowOfBoxes(numBoxes = 2, boxColor = Color.Red)
+    }
+}
+
+@Composable
+fun RowOfBoxes(
+    numBoxes: Int,
+    boxColor: Color = Color.Blue,
+    onClick: (boxNum: Int) -> Unit = { boxNum ->
+        Timber.d("Clicked box $boxNum")
+    },
+    modifier: Modifier = Modifier
+) {
+    Row(modifier = modifier.padding(10.dp)) {
+        for (index in 1..numBoxes) {
+            Button(
+                modifier = modifier
+                    .fillMaxWidth()
+                    .weight(1f),
+
+                colors = ButtonDefaults.buttonColors(
+                    backgroundColor = boxColor,
+                    contentColor = Color.White
+                ),
+
+                onClick = { onClick(index) }
+            ) {
+                Text("Navigate to Screen $index")
+            }
+            Spacer(modifier = Modifier.width(10.dp))
+        }
+    }
+}
+
+fun Button(modifier: Modifier, onClick: () -> Unit) {
+
 }
 
 @Preview
 @Composable
 fun NavAnimationScreen_Preview() {
-
+    NavAnimationScreen()
 }
