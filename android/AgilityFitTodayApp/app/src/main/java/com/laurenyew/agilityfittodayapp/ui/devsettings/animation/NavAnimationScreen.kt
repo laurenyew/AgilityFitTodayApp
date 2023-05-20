02d5cb9dc1ac8d08cff9fee5672ef04af9a72223
Lauren Yew <laurenyew@gmail.com>
2023-05-20 17:03:47 -0500

    TECH-6: Unable to make a button fill a whole screen. Limited by the box / surface structure under the scaffold. Also, we'd need to make it a composable screen rather than an activity. The activity flow has its own transitions which don't interact with compose.

--- android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/devsettings/animation/NavAnimationScreen.kt
+++ android/AgilityFitTodayApp/app/src/main/java/com/laurenyew/agilityfittodayapp/ui/devsettings/animation/NavAnimationScreen.kt
@@ -2,16 +2,26 @@ package com.laurenyew.agilityfittodayapp.ui.devsettings.animation
 
 import android.content.Context
 import android.content.Intent
+import androidx.compose.animation.core.animateFloat
+import androidx.compose.animation.core.tween
+import androidx.compose.animation.core.updateTransition
+import androidx.compose.foundation.background
+import androidx.compose.foundation.layout.Box
 import androidx.compose.foundation.layout.Column
 import androidx.compose.foundation.layout.Row
 import androidx.compose.foundation.layout.Spacer
-import androidx.compose.foundation.layout.fillMaxWidth
+import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.padding
 import androidx.compose.foundation.layout.width
 import androidx.compose.material.Button
 import androidx.compose.material.ButtonDefaults
+import androidx.compose.material.Surface
 import androidx.compose.material.Text
 import androidx.compose.runtime.Composable
+import androidx.compose.runtime.getValue
+import androidx.compose.runtime.mutableStateOf
+import androidx.compose.runtime.remember
+import androidx.compose.runtime.setValue
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.graphics.Color
 import androidx.compose.ui.graphics.toArgb
@@ -24,10 +34,22 @@ import androidx.compose.ui.unit.dp
  */
 @Composable
 fun NavAnimationScreen() {
-    Column {
-        RowOfBoxes(numBoxes = 1)
-        RowOfBoxes(numBoxes = 3, boxColor = Color.DarkGray)
-        RowOfBoxes(numBoxes = 2, boxColor = Color.Red)
+    Surface(
+        modifier = Modifier
+            .fillMaxSize()
+            .background(Color.Yellow)
+    ) {
+        Box(
+            Modifier
+                .fillMaxSize()
+                .background(Color.Cyan)
+        ) {
+            Column {
+                RowOfBoxes(numBoxes = 1)
+                RowOfBoxes(numBoxes = 3, boxColor = Color.DarkGray)
+                RowOfBoxes(numBoxes = 2, boxColor = Color.Red)
+            }
+        }
     }
 }
 
@@ -38,12 +60,23 @@ fun RowOfBoxes(
     modifier: Modifier = Modifier
 ) {
     val context = LocalContext.current
+    var isButtonFillScreen by remember { mutableStateOf(false) }
+    val transition =
+        updateTransition(targetState = isButtonFillScreen, label = "button_fill_screen")
+
+    val buttonSize by transition.animateFloat(
+        transitionSpec = { tween(durationMillis = 2000) },
+        label = "button_height",
+        targetValueByState = { isLarge ->
+            if (isLarge) 1f else .25f
+        }
+    )
+
     Row(modifier = modifier.padding(10.dp)) {
         for (index in 1..numBoxes) {
             Button(
                 modifier = modifier
-                    .fillMaxWidth()
-                    .weight(1f),
+                    .fillMaxSize(buttonSize),
 
                 colors = ButtonDefaults.buttonColors(
                     backgroundColor = boxColor,
@@ -51,7 +84,8 @@ fun RowOfBoxes(
                 ),
 
                 onClick = {
-                    openNavActivity(context = context, color = boxColor, screenNum = index)
+                    isButtonFillScreen = !isButtonFillScreen
+//                    openNavActivity(context = context, color = boxColor, screenNum = index)
                 }
             ) {
                 Text("Navigate to Screen $index")
